0f8ef268bc221e7f1aa46149173b038f56a6d5eb
Souradip <dgsouro77@gmail.com>
2024-05-16 02:05:24 +0530

    Added the login and signup routes

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 export const signup = async(req, res, next) => {
     try {
         const {fullName,username,password,confirmPassword,gender}=req.body;
@@ -30,6 +31,8 @@ export const signup = async(req, res, next) => {
          })
 
          if(newUser) {
+
+           generateTokenAndSetCookie(newUser._id,res);
             await newUser.save();
          res.status(201).json({
             _id:newUser._id,
@@ -50,8 +53,27 @@ export const signup = async(req, res, next) => {
     }
 }
 
-export const login = (req, res, next) => {
-    console.log("loginUser");
+export const login = async (req, res, next) => {
+    try {
+        const {username,password} = req.body;
+        const user= await User.findOne({username});
+        const isPasswordCorrect = await bcrypt.compare(password,user?.password || "");
+
+        if(!user || !isPasswordCorrect ) {
+              return res.status(400).json({error:"Invalid username or password"}); 
+        }
+        generateTokenAndSetCookie(user._id,res);
+
+        res.status(200).json({
+            _id:user._id,
+            fullName:user.fullName,
+            username:user.username,
+            profilePic:user.profilePic,
+        });
+    } catch (error) {
+        console.log("Error in login controller",error.message);
+        res.status(500).json({error:"Internal Server Error"});
+    }
 }
 
 export const logout = (req, res, next) => {
