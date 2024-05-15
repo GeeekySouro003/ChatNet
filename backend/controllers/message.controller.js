2a308049eac30a322638d33ad12686bfa8967f90
Souradip <dgsouro77@gmail.com>
2024-05-16 03:08:15 +0530

    Added the messages endpoints

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -38,3 +38,22 @@ export const sendMessage = async (req,res) => {
         res.status(500).json({error:"Internal Server Error"});
     }
 }
+
+export const getMessages = async(req, res) => {
+    try {
+        const {id:userToChatId}=req.params;
+        const senderId = req.user._id;
+
+        const conversation=await Conversation.findOne({
+            participants:{$all:[senderId,userToChatId]},
+        }).populate("messages");
+       
+        if(!conversation) return res.status(200).json([]);
+        const messages=conversation.messages;
+
+        res.status(200).json(messages);
+    } catch (error) {
+        console.log("Error in getMessages controller: " ,error.message)
+        res.status(500).json({error:"Internal Server Error"});
+    }
+    }
