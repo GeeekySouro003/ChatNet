60f48e88136b6ce3873bb2c3008c52e5ec0eeed6
Souradip <dgsouro77@gmail.com>
2024-05-16 03:20:50 +0530

    Updated the backend with the models rouets and others

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import authRoutes from '../backend/routes/auth.routes.js'
 import messageRoutes from '../backend/routes/message.routes.js'
+import userRoutes from '../backend/routes/user.routes.js'
 import connectToMongoDB from './db/connectToMongoDB.js';
 const app=express();
 const PORT=process.env.PORT || 5000;
@@ -10,8 +11,9 @@ const PORT=process.env.PORT || 5000;
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
-app.use("/api/auth",authRoutes)
-app.use("/api/messages",messageRoutes)
+app.use("/api/auth",authRoutes);
+app.use("/api/messages",messageRoutes);
+app.use("/api/users",userRoutes);
 
 app.listen(PORT, () => {
     connectToMongoDB();
