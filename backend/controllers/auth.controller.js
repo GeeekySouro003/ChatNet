1b33851581e7af595423fb39696f6ef8cad52c2f
Souradip <dgsouro77@gmail.com>
2024-05-16 02:10:01 +0530

    Updated the logout routes in backend

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -77,5 +77,11 @@ export const login = async (req, res, next) => {
 }
 
 export const logout = (req, res, next) => {
-    console.log("logoutUser");
+    try {
+        res.cookie("jwt","",{maxAge:0})
+        res.status(200).json({"message":"Logged out successfully"});
+    } catch (error) {
+        console.log("Error in logout controller",error.message);
+        res.status(500).json({error:"Internal Server Error"});
+    }
 }
