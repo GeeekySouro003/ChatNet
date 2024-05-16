31eed42bfa014dd12e0dd0e55f467aa3b02dcf31
Souradip <dgsouro77@gmail.com>
2024-05-16 14:24:49 +0530

    Updated the frotend with login singup pages and basic layout

---
+++ frontend/frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
+function App() {
+  
+
+  return (
+    <>
+     <div className='p-4 h-screen flex items-center justify-center'>
+     <Signup/>
+     </div>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/frontend/src/pages/signup/Signup.jsx
@@ -0,0 +1,123 @@
+import React from 'react'
+import GenderCheckbox from './GenderCheckbox'
+
+const Signup = () => {
+  return (
+    <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+ 
+     <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter
+     backdrop-blur-lg bg-opacity-0'>
+      <h1 className='text-3xl font-semibold text-center text-gray-300'>SignUp
+      <span className='text-blue-500'>ChatNet</span>
+      </h1>
+      
+      <form>
+        <div>
+        <label className='label p-2'>
+          <span className='text-base label-text'>Full Name</span>
+        </label>
+        <input type='text' placeholder='Mahi Kumar' className='w-full input input-bordered h-10' />
+        </div>
+
+        <div>
+         <label className='label p-2'>
+           <span className='text-base label-text'>Username</span>
+          </label> 
+        <input type='text' placeholder='msd007' className='w-full input input-bordered h-10'/>
+        </div>
+         <div>
+         <label className='label'>
+          <span className='text-base label-text'>Password</span>
+         </label>
+         <input type='password' placeholder='Enter Password' className='w-full input input-bordered h-10' />
+        </div>   
+        <div>
+        <label className='label'>
+          <span className='text-base label-text'>Confirm Password</span>
+        </label>
+        <input type='password' placeholder='Confirm Password' className='w-full input input-bordered h-10' />
+        </div>
+     
+     {/*GENDER CHECKBOX*/}
+     <GenderCheckbox/>
+
+     <a href='#' className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'>
+        {"Don't"} have an account?
+       </a>
+
+       <div>
+        <button className='btn btn-block btn-sm mt-2 border border-slate-700'>Sign Up</button>
+       </div>
+
+      </form>
+     </div>
+    </div>
+  )
+}
+
+export default Signup
+
+
+
+
+
+
+// STARTER CODE F0R THE SIGNUP COMPONENT
+
+// import GenderCheckbox from './GenderCheckbox'
+
+// const Signup = () => {
+//   return (
+//     <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+ 
+//      <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter
+//      backdrop-blur-lg bg-opacity-0'>
+//       <h1 className='text-3xl font-semibold text-center text-gray-300'>SignUp
+//       <span className='text-blue-500'>ChatNet</span>
+//       </h1>
+      
+//       <form>
+//         <div>
+//         <label className='label p-2'>
+//           <span className='text-base label-text'>Full Name</span>
+//         </label>
+//         <input type='text' placeholder='Mahi Kumar' className='w-full input input-bordered h-10' />
+//         </div>
+
+//         <div>
+//          <label className='label p-2'>
+//            <span className='text-base label-text'>Username</span>
+//           </label> 
+//         <input type='text' placeholder='msd007' className='w-full input input-bordered h-10'/>
+//         </div>
+//          <div>
+//          <label className='label'>
+//           <span className='text-base label-text'>Password</span>
+//          </label>
+//          <input type='password' placeholder='Enter Password' className='w-full input input-bordered h-10' />
+//         </div>   
+//         <div>
+//         <label className='label'>
+//           <span className='text-base label-text'>Confirm Password</span>
+//         </label>
+//         <input type='password' placeholder='Confirm Password' className='w-full input input-bordered h-10' />
+//         </div>
+     
+//      {/*GENDER CHECKBOX*/}
+//      <GenderCheckbox/>
+
+//      <a href='#' className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'>
+//         {"Don't"} have an account?
+//        </a>
+
+//        <div>
+//         <button className='btn btn-block btn-sm mt-2 border border-slate-700'>Sign Up</button>
+//        </div>
+
+//       </form>
+//      </div>
+//     </div>
+//   )
+// }
+
+// export default Signup
