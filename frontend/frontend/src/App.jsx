d328c86c9cb3458b51ebd7820edf14ec7bea33c6
Souradip <dgsouro77@gmail.com>
2024-05-19 14:06:19 +0530

    Logout functionality working properly

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -12,8 +12,8 @@ const {authUser} =useAuthContext();
     <>
      <div className='p-4 h-screen flex items-center justify-center'>
      <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/login' element={<Login />} />
+      <Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} /> } />
+      <Route path='/login' element={authUser ? <Navigate to='/' /> : <Login />} />
       <Route path='/signup' element={authUser ? <Navigate to="/" /> : <Signup />} />
      </Routes>
      <Toaster />
