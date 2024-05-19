83784ea04a58f87a9e7867b8f9bcbfbd9045551b
Souradip <dgsouro77@gmail.com>
2024-05-19 13:40:26 +0530

    Added the authContext

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,19 +1,20 @@
-import { Routes,Route } from 'react-router-dom'
+import { Routes,Route, Navigate } from 'react-router-dom'
 import './App.css'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import Signup from './pages/signup/Signup'
 import { Toaster } from 'react-hot-toast'
+import { useAuthContext } from './context/AuthContext'
 function App() {
   
-
+const {authUser} =useAuthContext();
   return (
     <>
      <div className='p-4 h-screen flex items-center justify-center'>
      <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/login' element={<Login />} />
-      <Route path='/signup' element={<Signup />} />
+      <Route path='/signup' element={authUser ? <Navigate to="/" /> : <Signup />} />
      </Routes>
      <Toaster />
      </div>
--- frontend/frontend/src/hooks/useSignup.js
+++ frontend/frontend/src/hooks/useSignup.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
 const useSignup = () => {
 
     
     const [loading,setLoading] =useState(false);
+    const{setAuthUser} = useAuthContext();
 
     const signup = async ({fullName,username,password,confirmPassword,gender}) => {
         const success =handleInputErrors({fullName,username,password,confirmPassword,gender})
@@ -18,7 +20,14 @@ const useSignup = () => {
             })
 
         const data=await res.json();
-        console.log(data)
+        if(data.error) {
+
+            throw new Error(data.error);
+        }
+        localStorage.setItem("chat-user",JSON.stringify(data));
+        setAuthUser(data);
+        
+       // console.log(data)
         } catch (error) {
             toast.error(error.message);
         }
