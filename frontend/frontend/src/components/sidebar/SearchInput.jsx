c6cca94db833ba62d23077eaa526600f33c37637
Souradip <dgsouro77@gmail.com>
2024-06-03 19:30:20 +0530

    Fixed bugs of search bar

--- frontend/frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/frontend/src/components/sidebar/SearchInput.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoSearchSharp } from "react-icons/io5";
+import useConversation from '../../zustand/useConversation';
+import toast from 'react-hot-toast';
+import useGetConversation from '../../hooks/useGetConversation';
 
 const SearchInput = () => {
+  const [search,setSearch]=useState("");
+  const {setSelectedConversation} = useConversation()
+  const {conversations} =useGetConversation()
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!search) return;
+    if(search.length < 3) {
+      return toast.error("Search term must be at least 3 characters")
+    }
+    const conversation = conversations.find((c)=> c.fullName.toLowerCase().includes(search.toLowerCase()));
+
+    if(conversation) {
+      setSelectedConversation(conversation);
+      setSearch("");
+    }
+    else 
+      toast.error("No such user is found");
+  }
   return (
-    <form className='flex items-center gap-2'>
-     <input type='text' placeholder='Search...' className='input input-bordered rounded-full' />
+    <form onSubmit={handleSubmit} className='flex items-center gap-2'>
+     <input type='text' placeholder='Search...' className='input input-bordered rounded-full'
+     value={search}
+     onChange={(e) => setSearch(e.target.value)}
+     />
      <button type='submit' className='btn btn-circle bg-sky-500 text-white'>
       <IoSearchSharp className='w-6 h-6 outline-none'/>
     </button>    
