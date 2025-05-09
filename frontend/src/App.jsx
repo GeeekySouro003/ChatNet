d5951e27ad4627e7b6ac46322b9701ffcafe9a52
Souradip Dasgupta <dgsouro77@gmail.com>
2025-05-09 23:35:01 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,7 @@ const {authUser} =useAuthContext();
     <>
      <div className='p-4 h-screen flex items-center justify-center'>
      <Routes>
+       
       <Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} /> } />
       <Route path='/login' element={authUser ? <Navigate to='/' /> : <Login />} />
       <Route path='/signup' element={authUser ? <Navigate to="/" /> : <Signup />} />
